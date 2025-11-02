53da5a6823d8f81698302958a9acd1591f2b984f
paul010 <21501097@qq.com>
2025-11-02 17:20:48 +0800

    Initial Commit

---
+++ DailyCosmos/DailyCosmosApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyCosmosApp.swift
+//  DailyCosmos
+//
+//  Created by Paul on 2025/11/2.
+//
+
+import SwiftUI
+
+@main
+struct DailyCosmosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
